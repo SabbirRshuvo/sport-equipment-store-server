e3fc10086f9b5e50a02fba732af53d36e319c420
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-03-04 02:12:13 +0600

    equipment crud is done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -23,6 +23,42 @@ async function run() {
   try {
     await client.connect();
     console.log("mongodb is connected");
+
+    const database = client.db("addEquipment");
+    const usersCollection = database.collection("store");
+
+    // create a users to server to client
+    app.post("/add_equipment", async (req, res) => {
+      const query = req.body;
+      console.log("created new users", query);
+      const result = await usersCollection.insertOne(query);
+      res.send(result);
+    });
+
+    //display this equipment
+    app.get("/add_equipment", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // find a user
+    app.get("/add_equipment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    // delete user
+    app.delete("/add_equipment/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/", async (req, res) => {
       res.send("sports equipment store server is on the server");
     });
