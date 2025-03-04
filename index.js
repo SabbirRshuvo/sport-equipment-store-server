13d18da90dfaa9eb2126e46559deb8f845889adb
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-03-05 03:09:53 +0600

    something is changes

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ async function run() {
       res.send(result);
     });
 
+    // update the user crud operation
+
     app.put("/add_equipment/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
